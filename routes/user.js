85d2cbfe693ed50df3aaa6fa311f099b74214827
beatrisiis <isisbeatrizsp2010@hotmail.com>
2024-04-05 14:35:28 -0300

    Modificações no cadastro e adição do endpoint de edição de usuário

--- routes/user.js
+++ routes/user.js
@@ -19,7 +19,25 @@ router.post("/cadastro", (req, res) => {
                 console.log(err);
                 res.status(500).json({ message: "Erro ao registrar usuário" });
             } else {
-                res.status(201).json({ message: "Usuário registrado com sucesso" });
+
+                // Após a edição, busque o usuário atualizado
+               db.query(
+                "SELECT * FROM user WHERE email= ?",
+                [email],
+                (err, results) => {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).json({ message: "Erro ao buscar usuário cadastrado" });
+                    } else if (results.length === 0) {
+                        res.status(404).json({ message: "Usuário não encontrado " });
+                    } else {
+              
+                        const user = results[0];
+                        res.status(201).json({ message: "Usuário editado com sucesso", user });
+                    }
+                }
+            );
+                
             }
         }
     );
@@ -43,6 +61,42 @@ router.post("/login", (req, res) => {
                 console.log("Usuário encontrado")
                 const user = results[0]; 
                 res.status(200).json({ message: "login realizado", user });
+            }
+        }
+    );
+});
+
+// endpoint de edição de usuário
+router.put("/editar/:id", (req, res) => {
+    const userId = req.params.id;
+    const { username, email, password } = req.body;
+
+    // atualiza os dados do usuário no banco 
+    db.query( 
+        "UPDATE user SET username = ?, email = ?, password = ? WHERE id = ?",
+        [username, email, password, userId],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ message: "Erro ao editar usuário" });
+            } else {
+                // Após a edição, busque o usuário atualizado
+                db.query(
+                    "SELECT * FROM user WHERE id = ?",
+                    [userId],
+                    (err, results) => {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).json({ message: "Erro ao buscar usuário atualizado" });
+                        } else if (results.length === 0) {
+                            res.status(404).json({ message: "Usuário não encontrado após edição" });
+                        } else {
+                  
+                            const user = results[0];
+                            res.status(200).json({ message: "Usuário editado com sucesso", user });
+                        }
+                    }
+                );
             }
         }
     );
